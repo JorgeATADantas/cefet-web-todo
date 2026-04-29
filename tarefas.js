60d887df5a77accea625c911085787f6cf9c08d0
Jorge Dantas <jatad.tech@gmail.com>
2026-04-29 10:09:25 -0300

    Exercicio1-concluido

--- tarefas.js
+++ tarefas.js
@@ -17,5 +17,34 @@ let tarefas =  [
         categoria:  'estudos',
         realizada:  false
     }
-
 ];
+
+//Exercício 1: Carregar tarefas existentes
+
+//Criando a função insereTarefaNaPagina 
+function insereTarefaNaPagina(tarefa){
+    // 1. Solicitamos ao document a criação de um elemento
+    let novaTarefaEl = document.createElement('li'); 
+    
+    // 2. Configurando-lo (atributos, id, classes etc.)
+    //add a classe item-tarefa
+    novaTarefaEl.classList.add('item-tarefa');
+    //add a classe categoria-NOME          
+    novaTarefaEl.classList.add(`categoria-${tarefa.categoria}`);
+    //add o nome ao HTML   
+    novaTarefaEl.innerHTML = tarefa.nome;
+    if (tarefa.realizada)
+        novaTarefaEl.classList.add('marcado');
+  
+    // 3. Inserção do elemento na árvore DOM
+    let containerEl = document.querySelector('#lista-tarefas');
+    containerEl.appendChild(novaTarefaEl);
+}
+
+//Excluindo as tarefas existentes
+let limparTarefasEl = document.querySelector('#lista-tarefas');
+limparTarefasEl.innerHTML = '';
+
+//Inserindo as tarefas na página
+tarefas.forEach(insereTarefaNaPagina)
+
