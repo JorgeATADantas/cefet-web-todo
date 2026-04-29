5045899cc2ea4a40a70830bc96c3412956df343e
Jorge Dantas <jatad.tech@gmail.com>
2026-04-29 08:55:51 -0300

    Exercicio0-concluido

---
+++ tarefas.js
@@ -0,0 +1,21 @@
+
+//Exercício 0: Representar tarefas
+//tarefa é uma lista de objetos
+let tarefa =  [
+    {
+        nome:       'Comprar leite',
+        categoria:  'compras',
+        realizada:  false
+    },
+    {
+        nome:       'Escutar Chimbinha',
+        categoria:  'lazer',
+        realizada:  true
+    },
+    {
+        nome:       'Fazer trabalho de BD',
+        categoria:  'estudos',
+        realizada:  false
+    }
+
+];
