a536da34f3724d928a6e86197fca4509f365cdb7
Jorge Dantas <jatad.tech@gmail.com>
2026-04-30 21:03:47 -0300

    Exercicio2-concluido

--- tarefas.js
+++ tarefas.js
@@ -24,21 +24,21 @@ let tarefas =  [
 //Criando a função insereTarefaNaPagina 
 function insereTarefaNaPagina(tarefa){
     // 1. Solicitamos ao document a criação de um elemento
-    let novaTarefaEl = document.createElement('li'); 
+    let tarefaEl = document.createElement('li'); 
     
     // 2. Configurando-lo (atributos, id, classes etc.)
     //add a classe item-tarefa
-    novaTarefaEl.classList.add('item-tarefa');
+    tarefaEl.classList.add('item-tarefa');
     //add a classe categoria-NOME          
-    novaTarefaEl.classList.add(`categoria-${tarefa.categoria}`);
+    tarefaEl.classList.add(`categoria-${tarefa.categoria}`);
     //add o nome ao HTML   
-    novaTarefaEl.innerHTML = tarefa.nome;
+    tarefaEl.innerHTML = tarefa.nome;
     if (tarefa.realizada)
-        novaTarefaEl.classList.add('marcado');
+        tarefaEl.classList.add('marcado');
   
     // 3. Inserção do elemento na árvore DOM
     let containerEl = document.querySelector('#lista-tarefas');
-    containerEl.appendChild(novaTarefaEl);
+    containerEl.appendChild(tarefaEl);
 }
 
 //Excluindo as tarefas existentes
@@ -46,5 +46,36 @@ let limparTarefasEl = document.querySelector('#lista-tarefas');
 limparTarefasEl.innerHTML = '';
 
 //Inserindo as tarefas na página
-tarefas.forEach(insereTarefaNaPagina)
+tarefas.forEach(insereTarefaNaPagina);
 
+//Exercício 2: Incluir uma nova tarefa
+function insereNovaTarefa(){
+    let nomeNovaTarefaE1            = document.querySelector('#nova-tarefa-nome');
+    let nomeNovaTarefaValor         = nomeNovaTarefaE1.value;
+    let categoriaNovaTarefaE1       = document.querySelector('#nova-tarefa-categoria');
+    let categoriaNovaTarefaValor    = categoriaNovaTarefaE1.value;
+    let realizadaNovaTarefaE1       = false;
+
+    //Monta a tarefa
+    let tarefa = {
+        nome:       nomeNovaTarefaValor,
+        categoria:  categoriaNovaTarefaValor,
+        realizada:  realizadaNovaTarefaE1
+    };
+
+    //Adiciona na lista de tarefas
+    tarefas.push(tarefa);
+
+    //Adiciona na página
+    insereTarefaNaPagina(tarefa);
+
+    //Apaga campo
+    nomeNovaTarefaE1.value = '';
+
+    //Puxar o foco
+    nomeNovaTarefaE1.focus();
+}
+
+// Clicar no botão + para adicionar a tarefa
+let botaoAdicionar = document.querySelector('#incluir-nova-tarefa');
+botaoAdicionar.addEventListener('click', insereNovaTarefa);
